37361961afd8a696453ac76667d701e527b54985
shiraga-riku <riku125@icloud.com>
2020-02-20 18:13:57 +0900

    likesコントローラー作成　アクションとルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets, only:[:new,:create,:show,:destroy] do
+    resources :likes, only:[:create,:destroy]
     resources :comments, only: :create
   end
 end
